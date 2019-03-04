76764c3b6deaaf7e3941476c52bd5b610af0030a
Kryword <kryword@gmail.com>
2019-03-04 13:45:30 +0100

    Añadido submenú con soluciones guardadas en Parse

--- vrpParse.js
+++ vrpParse.js
@@ -29,3 +29,41 @@ function guardaSolucionParse(solucion){
         alert("Se produjo un error al subir la solución a Parse: " + error.number + " " + error.message);
     });
 }
+
+function listaSolucionesParse(){
+    $('.vrpSolutions').remove();
+    const Store = Parse.Object.extend("Soluciones");
+    var query = new Parse.Query(Store);
+    query.find().then((results)=>{
+        for (var i = 0; i < results.length; i++) {
+            let solucion = results[i];
+            var nuevoElemento = $('<a />',{
+                text: solucion.get('name'),
+                title: solucion.get('name'),
+                href: '#',
+                class: 'vrpSolutions',
+            })
+            nuevoElemento.on('click', function (){
+                cargaSolucion(solucion.id)
+            });
+            nuevoElemento.appendTo('#sideNav');
+
+            console.log("Solucion: " + solucion.get('name') + ", " + solucion.get('vehicleRouteList') + ', ' + solucion.get('distance'));
+        }
+        $("#loadingImg").hide();
+    });
+}
+
+function cargaSolucion(id){
+    alert("Cargando solución con id = " + id);
+}
+
+function openNav() {
+    $("#loadingImg").show();
+    listaSolucionesParse();
+    document.getElementById("sideNav").style.width = "300px";
+}
+
+function closeNav(){
+    document.getElementById("sideNav").style.width = "0";
+}
